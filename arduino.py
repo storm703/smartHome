6f4a3efcec14b5d1b6c5a5fb38578cc40a8bdece
Bi0s7 <paul-rabich@web.de>
2020-04-30 12:22:15 +0200

    Arduino Klasse v0

---
+++ arduino.py
@@ -0,0 +1,17 @@
+class Arduino_Rolladen:
+
+    def __init__(self, id):
+        self.ID = id
+        self.rolladen_hight = none
+
+    def set_hight(self, hight):
+        #an arduino senden
+        pass
+
+    def get_hight(self):
+        hight = 0
+        #höhe abfragen
+        return get_hight
+
+    def get_id(self):
+        return self.ID

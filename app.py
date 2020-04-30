83839cd33d307dc998c73893b3ded1138b02a9df
storm703 <mario-gomez-andreu@outlook.de>
2020-04-30 12:41:44 +0200

    lul

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask, render_template
+from arduino import Arduino_Rolladen
 
 app = Flask(__name__)
 
+ard = [Arduino_Rolladen(0, "Wohnzimmer 1")]
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+
+    return render_template('index.html', array = ard)
 
 if __name__ == "__main__":
     app.run(debug= True)

c14e28b6dcd4a7d57f803085733d83d70a1f0b72
Bi0s7 <paul-rabich@web.de>
2020-04-30 12:54:51 +0200

    v7

--- server.py
+++ server.py
@@ -1,4 +1,12 @@
 import socket
 
 server_addr = ('127.0.0.1', 7500)
-sock = 
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind("localhost", 12345)
+sock.listen(1)
+
+while True:
+    (clientsocket, address) = sock.accept()
+    mes = sock.recv(1024)
+    clientsocket.sendto(b(mes))
+    
